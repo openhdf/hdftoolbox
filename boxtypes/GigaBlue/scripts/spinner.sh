58d3dcf2eceb8365ad5bd40c1b1cadcb99417af8
Koivo <koivo@HDFreaks>
2012-11-20 16:19:37 +0100

    small visual changes

--- boxtypes/GigaBlue/scripts/spinner.sh
+++ boxtypes/GigaBlue/scripts/spinner.sh
@@ -9,7 +9,7 @@ echo "Download and install update"
  	opkg update > /dev/null 2>&1
 	opkg upgrade
 	echo
-	wget http://addons.hdfreaks.cc/feeds/enigma2-plugins-update-HDFreaks_SpinnerupdateGigaBlue.tar.gz
+	wget -q http://addons.hdfreaks.cc/feeds/enigma2-plugins-update-HDFreaks_SpinnerupdateGigaBlue.tar.gz
 	echo
 	echo "extract files"
 	rm -f /usr/share/enigma2/skin_default/spinner/wait*.*
@@ -19,5 +19,4 @@ echo "Download and install update"
 	echo
 	echo "Update Done ... Please reboot your Box now!"
 	echo
-	##wget -q -O /tmp/.message.txt "http://127.0.0.1/api/message?text=Update%20done%20...%20please%20reboot%20your%20STB%20now%20...&type=2" &  > /dev/null 2>&1
 exit 0 
