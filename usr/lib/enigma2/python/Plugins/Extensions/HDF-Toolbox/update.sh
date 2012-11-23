6e412fd842560d3edfe94bd042573360cc1c0106
Koivo <koivo@HDFreaks>
2012-11-23 11:51:54 +0100

    small optic change

--- usr/lib/enigma2/python/Plugins/Extensions/HDF-Toolbox/update.sh
+++ usr/lib/enigma2/python/Plugins/Extensions/HDF-Toolbox/update.sh
@@ -1,31 +1,31 @@
 #!/bin/sh
-echo "Download new version"
+echo "Download new version of HDF-Toolbox"
 IP=hdfreaks.cc
 ping -c 1 $IP  > /dev/null 2>&1
 if [ $? == 0 ]
 	then
-		wget http://addons.hdfreaks.cc/HDF-Toolbox/release.tgz -O /usr/lib/enigma2/python/Plugins/Extensions/release.tgz
-		echo "download complete"
+		wget -q http://addons.hdfreaks.cc/feeds/enigma2-plugin-extensions-hdftoolbox_v5_all.ipk -O /tmp/enigma2-plugin-extensions-hdftoolbox_v5_all.ipk
+		echo
+		echo "Download complete"
+		echo
 	else
-  		echo "update failed"
+  		echo "Sorry ... Update failed"
 fi;
 
-if [ -f /usr/lib/enigma2/python/Plugins/Extensions/release.tgz ]; then 
+if [ -f /tmp/enigma2-plugin-extensions-hdftoolbox_v5_all.ipk ]; then 
 	rm -fr /usr/lib/enigma2/python/Plugins/Extensions/HDF-Toolbox/boxtypes/ > /dev/null 2>&1
 	rm -fr /usr/lib/enigma2/python/Plugins/Extensions/HDF-Toolbox/menu > /dev/null 2>&1
 	rm -fr /usr/lib/enigma2/python/Plugins/Extensions/HDF-Toolbox/scripts > /dev/null 2>&1
 	rm -fr /usr/lib/enigma2/python/Plugins/Extensions/HDF-Toolbox/main.cfg > /dev/null 2>&1
-	cp /usr/lib/enigma2/python/Plugins/Extensions/release.tgz /usr/lib/enigma2/python/Plugins/Extensions/HDF-Toolbox/release.tgz
-	cd /usr/lib/enigma2/python/Plugins/Extensions/HDF-Toolbox
-	tar xzf release.tgz -C /
-	rm release.tgz
-	rm /usr/lib/enigma2/python/Plugins/Extensions/release.tgz
+	opkg install /tmp/enigma2-plugin-extensions-hdftoolbox_v5_all.ipk
+	#cp /usr/lib/enigma2/python/Plugins/Extensions/release.tgz /usr/lib/enigma2/python/Plugins/Extensions/HDF-Toolbox/release.tgz
+	#cd /usr/lib/enigma2/python/Plugins/Extensions/HDF-Toolbox
+	#tar xzf release.tgz -C /
+	#rm release.tgz
+	rm /tmp/enigma2-plugin-extensions-hdftoolbox_v5_all.ipk
 	date > /usr/lib/enigma2/python/Plugins/Extensions/HDF-Toolbox/.lastupdate.log
 echo
-echo
 echo "please reboot enigma now"
-echo
-	#wget -q -O /tmp/.message.txt "http://127.0.0.1/api/message?text=Update%20done%20...%20please%20reboot%20your%20STB%20now%20...&type=2" &  > /dev/null 2>&1
 echo
 else
 	echo "sorry there is a problem"
