c6ad532202863107c61657c84fd469ec90569508
unknown <steueran@ZWIN019.crossen.sas-automotive.com>
2012-12-29 12:21:53 +0100

    von PLi auf opkg geändert

---
+++ usr/lib/enigma2/python/Plugins/Extensions/HDF-Toolbox/boxtypes/TM800/scripts/opkg_update.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+cd /tmp
+echo "Download and install update"
+
+	echo
+	echo "starting okgp update & upgrade"
+    opkg update  > /dev/null 2>&1
+	opkg upgrade
+	echo
+	echo "Update Done ... Please reboot your Box now!"
+	echo
+
+exit 0 
