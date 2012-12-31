766cdf3d9251653406de6b324b354484e27b04ac
koivo <koivo@HDFreaks>
2012-12-31 20:27:00 +0100

    wechsel auf ein Spinnerupdate für alle Boxen

--- usr/lib/enigma2/python/Plugins/Extensions/HDF-Toolbox/boxtypes/ET9x00/scripts/spinner.sh
+++ usr/lib/enigma2/python/Plugins/Extensions/HDF-Toolbox/boxtypes/ET9x00/scripts/spinner.sh
@@ -1,25 +1,20 @@
 #!/bin/sh
 
 cd /tmp
-echo "Download and install update"
-
-	echo
-	echo "Boxtype et9x00/et6x00"
+	echo "start OpenHDF spinnerupdate"
 	echo
 	echo "starting okgp update & upgrade"
-    	opkg update > /dev/null 2>&1
-		opkg upgrade
+ 	opkg update > /dev/null 2>&1
+	opkg upgrade
 	echo
-	cd /tmp
-	echo "download & extract spinnerupdate"
-	wget -q http://addons.hdfreaks.cc/feeds/enigma2-plugins-update-HDFreaks_SpinnerupdateET9x00.tar.gz
+	echo "download OpenHDF spinnerupdate"
+	wget -q http://addons.hdfreaks.cc/feeds/enigma2-plugins-update-HDFreaks_SpinnerupdateOpenHDF.tar.gz
 	echo
-	echo "extract files"
+	echo "extract spinnerupdate"
 	cd /
-	tar xzvf /tmp/enigma2-plugins-update-HDFreaks_SpinnerupdateET9x00.tar.gz > /dev/null 2>&1
-	rm -f /tmp/enigma2-plugins-update-HDFreaks_SpinnerupdateET9x00.tar.gz > /dev/null 2>&1
+	tar xzvf /tmp/enigma2-plugins-update-HDFreaks_SpinnerupdateOpenHDF.tar.gz > /dev/null 2>&1
+	rm -f /tmp/enigma2-plugins-update-HDFreaks_SpinnerupdateOpenHDF.tar.gz > /dev/null 2>&1
 	echo
-	echo "Update Done ... Please reboot your Box now!"
-	echo
+	echo "update done ... please reboot your Box now!"
 
 exit 0 
--- usr/lib/enigma2/python/Plugins/Extensions/HDF-Toolbox/boxtypes/GigaBlue/scripts/spinner.sh
+++ usr/lib/enigma2/python/Plugins/Extensions/HDF-Toolbox/boxtypes/GigaBlue/scripts/spinner.sh
@@ -1,22 +1,20 @@
 #!/bin/sh
 
 cd /tmp
-echo "Download and install update"
-	echo
-	echo "Boxtype GigaBlue"
+	echo "start OpenHDF spinnerupdate"
 	echo
 	echo "starting okgp update & upgrade"
  	opkg update > /dev/null 2>&1
 	opkg upgrade
 	echo
-	wget -q http://addons.hdfreaks.cc/feeds/enigma2-plugins-update-HDFreaks_SpinnerupdateGigaBlue.tar.gz
+	echo "download OpenHDF spinnerupdate"
+	wget -q http://addons.hdfreaks.cc/feeds/enigma2-plugins-update-HDFreaks_SpinnerupdateOpenHDF.tar.gz
 	echo
-	echo "extract files"
-	rm -f /usr/share/enigma2/skin_default/spinner/wait*.*
+	echo "extract spinnerupdate"
 	cd /
-	tar xzvf /tmp/enigma2-plugins-update-HDFreaks_SpinnerupdateGigaBlue.tar.gz > /dev/null 2>&1
-	rm -f /tmp/enigma2-plugins-update-HDFreaks_SpinnerupdateGigaBlue.tar.gz > /dev/null 2>&1
+	tar xzvf /tmp/enigma2-plugins-update-HDFreaks_SpinnerupdateOpenHDF.tar.gz > /dev/null 2>&1
+	rm -f /tmp/enigma2-plugins-update-HDFreaks_SpinnerupdateOpenHDF.tar.gz > /dev/null 2>&1
 	echo
-	echo "Update Done ... Please reboot your Box now!"
-	echo
+	echo "update done ... please reboot your Box now!"
+
 exit 0 
