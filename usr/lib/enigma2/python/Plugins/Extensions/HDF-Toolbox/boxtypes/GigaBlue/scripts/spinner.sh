2e252c67f8a565ba2921175f6f3d5bd38e73a1db
koivo <koivo@HDFreaks>
2013-01-05 09:53:58 +0100

    vorher alten Spinner löschen

--- usr/lib/enigma2/python/Plugins/Extensions/HDF-Toolbox/boxtypes/GigaBlue/scripts/spinner.sh
+++ usr/lib/enigma2/python/Plugins/Extensions/HDF-Toolbox/boxtypes/GigaBlue/scripts/spinner.sh
@@ -11,10 +11,11 @@ cd /tmp
 	wget -q http://addons.hdfreaks.cc/feeds/enigma2-plugins-update-HDFreaks_SpinnerupdateOpenHDF.tar.gz
 	echo
 	echo "extract spinnerupdate"
+	rm -fr /usr/share/enigma2/skin_default/spinner/ > /dev/null 2>&1
 	cd /
 	tar xzvf /tmp/enigma2-plugins-update-HDFreaks_SpinnerupdateOpenHDF.tar.gz > /dev/null 2>&1
 	rm -f /tmp/enigma2-plugins-update-HDFreaks_SpinnerupdateOpenHDF.tar.gz > /dev/null 2>&1
 	echo
 	echo "update done ... please reboot your Box now!"
-
+	echo
 exit 0 
