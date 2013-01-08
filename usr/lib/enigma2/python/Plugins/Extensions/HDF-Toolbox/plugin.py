25aeb25cec0c4903d0a51fff2ebce2224206a0f8
unknown <steueran@ZWIN019.crossen.sas-automotive.com>
2013-01-08 17:22:48 +0100

    change boxtype Vu+Duo

--- usr/lib/enigma2/python/Plugins/Extensions/HDF-Toolbox/plugin.py
+++ usr/lib/enigma2/python/Plugins/Extensions/HDF-Toolbox/plugin.py
@@ -16,9 +16,9 @@ try:
         box = "ET4x00"
     #VU Boxen
     elif getBoxType() == "vuduo":
-        box = "VU+Duo"
+        box = "Vu+Duo"
     elif getBoxType() == "vusolo":
-        box = "VU+Solo"
+        box = "Vu+Solo"
     #GigaBlues
     elif getBoxType().startswith('gb'):
         box = "GigaBlue"
@@ -154,7 +154,7 @@ class Fantastic(Screen):
         <screen position="150,150" size="360,395" title="HDF Toolbox">
         <widget name="menu" position="10,10" size="340,340" scrollbarMode="showOnDemand" enableWrapAround="1" />
 		<ePixmap position="10,335" size="380,57" pixmap="/usr/lib/enigma2/python/Plugins/Extensions/HDF-Toolbox/banner.png" zPosition="1" alphatest="on" />
-        <eLabel text="Version .OE. 06.01.2013" position="205,315" size="990,45" font="Regular;12" valign="right" transparent="1" />
+        <eLabel text="Version .OE. 08.01.2013" position="205,315" size="990,45" font="Regular;12" valign="right" transparent="1" />
         </screen>"""
 
     def __init__(self, session, args = 0):
