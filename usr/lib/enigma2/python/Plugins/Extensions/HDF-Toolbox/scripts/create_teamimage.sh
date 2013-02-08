4a429e5d1707a42188ae9395333b7b82974b9893
Koivo <koivo@live.de>
2013-02-08 17:24:31 +0100

    some changes at teamimage script

--- usr/lib/enigma2/python/Plugins/Extensions/HDF-Toolbox/scripts/create_teamimage.sh
+++ usr/lib/enigma2/python/Plugins/Extensions/HDF-Toolbox/scripts/create_teamimage.sh
@@ -5,15 +5,19 @@ echo "This script create a teamimage ... almost but not complete"
 echo
 echo "please wait".
 echo
+echo "remove old plugins"
+opkg remove enigma2-plugin-extensions-remotestreamconvert enigma2-plugin-systemplugins-extnumberzap
+echo
 echo "install system plugins"
 opkg install enigma2-plugin-extensions-bootvideo-hdf enigma2-plugin-extensions-zaphistorybrowser.mod enigma2-plugin-extensions-virtualzap.mod
 opkg install enigma2-plugin-extensions-2ib enigma2-plugin-extensions-et-portal enigma2-plugin-extensions-openairplaymod
 opkg install enigma2-plugin-extensions-seekbarmod enigma2-plugin-extensions-webinterface-old-package enigma2-plugin-extensions-menusort
 opkg install enigma2-plugin-extensions-jobmanager enigma2-plugin-systemplugins-extnumberzap enigma2-plugin-systemplugins-crossepg
+opkg install enigma2-plugin-systemplugins-satelliteequipmentcontrol
 echo
 echo "install addons"
-opkg install enigma2-plugin-extensions-Burnseries enigma2-plugin-extensions-webradiofs
-opkg install enigma2-plugin-extensions-mediaportal enigma2-plugin-extensions-tvkino
+opkg install enigma2-plugin-extensions-Burnseries enigma2-plugin-extensions-webradiofs enigma2-plugin-extensions-skyanytime
+opkg install enigma2-plugin-extensions-mediaportal enigma2-plugin-extensions-tvkino enigma2-plugin-extensions-moviecut
 opkg install enigma2-plugin-extensions-songsto enigma2-plugin-extensions-putpattv enigma2-plugin-extensions-netzkino
 opkg install enigma2-plugin-extensions-myentertainment enigma2-plugin-extensions-movie2kserien
 opkg install enigma2-plugin-extensions-movie2kfilme enigma2-plugin-extensions-kinokiste enigma2-plugin-extensions-istream
@@ -21,6 +25,7 @@ opkg install enigma2-plugin-extensions-dokumonster enigma2-plugin-extensions-cin
 opkg install enigma2-plugin-extensions-epgrefresh_mod enigma2-plugin-extensions-yampmusicplayer
 opkg install enigma2-plugin-systemplugins-recordinfobar enigma2-plugin-extensions-icefilms enigma2-plugin-extensions-cubic_streamer
 opkg install enigma2-plugin-extensions-cooltvguide enigma2-plugin-extensions-pipservicerelation.mod
+opkg install enigma2-plugin-extensions-infobartunerstate
 echo
 echo "done ... but not complete"
 echo "please reboot your box now"
