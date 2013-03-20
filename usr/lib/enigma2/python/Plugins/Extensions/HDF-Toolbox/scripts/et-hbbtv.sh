973b8b78ccd32eb0c19675ca034a59f5e07488d0
Koivo <koivo@live.de>
2013-03-20 15:59:31 +0100

    install/remove script for et webbrowser to save more flashmemory

--- usr/lib/enigma2/python/Plugins/Extensions/HDF-Toolbox/scripts/et-hbbtv.sh
+++ usr/lib/enigma2/python/Plugins/Extensions/HDF-Toolbox/scripts/et-hbbtv.sh
@@ -1,20 +1,30 @@
 #!/bin/sh
 
 if [ -f /usr/local/browser/browser ]; then 
+	echo opkg list-installed | grep webbrowser
+	echo
 	echo "HbbTV Browser found ... remove ET9x00 HbbTV"
 	echo
-	opkg remove enigma2-plugin-extensions-et-hbbtv
+	opkg remove enigma2-plugin-extensions-et-webbrowser
 	echo
 	echo "done"
+	sync
 	echo
 	df -h | grep rootfs
 	echo
 else
 	echo "HbbTV Browser not found ... install ET9x00 HbbTV"
 	echo
-	opkg install enigma2-plugin-extensions-et-hbbtv
+	freespace=`df -h | grep rootfs | df -h | grep rootfs | cut -c 46-47`
+	freeneeded=10
+	if [ $freespace -ge $freeneeded ]; then
+		opkg install enigma2-plugin-extensions-et-webbrowser
+		echo "done"
+	else
+		echo "Sorry not enough space in flash to install Browser"
+	fi
 	echo
-	echo "done"
+	sync
 	echo
 	df -h | grep rootfs
 	echo
