1578cc37fcfa2be6a073d5ecae3e2707dc449b91
Koivo <koivo@live.de>
2013-03-24 21:22:48 +0100

    change teamimage script

--- usr/lib/enigma2/python/Plugins/Extensions/HDF-Toolbox/scripts/create_teamimage.sh
+++ usr/lib/enigma2/python/Plugins/Extensions/HDF-Toolbox/scripts/create_teamimage.sh
@@ -20,12 +20,12 @@ opkg install enigma2-plugin-extensions-Burnseries enigma2-plugin-extensions-webr
 opkg install enigma2-plugin-extensions-mediaportal enigma2-plugin-extensions-tvkino enigma2-plugin-extensions-moviecut
 opkg install enigma2-plugin-extensions-songsto enigma2-plugin-extensions-putpattv enigma2-plugin-extensions-netzkino
 opkg install enigma2-plugin-extensions-myentertainment enigma2-plugin-extensions-movie2kserien enigma2-plugin-extensions-enhancedpowersave
-opkg install enigma2-plugin-extensions-movie2kfilme enigma2-plugin-extensions-kinokiste enigma2-plugin-extensions-istream
+opkg install enigma2-plugin-extensions-movie2kfilme enigma2-plugin-extensions-flashexpander enigma2-plugin-extensions-tvcharts
 opkg install enigma2-plugin-extensions-dokumonster enigma2-plugin-extensions-cinestreamer enigma2-plugin-extensions-remotechannelstreamimport
 opkg install enigma2-plugin-extensions-epgrefresh_mod enigma2-plugin-extensions-yampmusicplayer enigma2-plugin-extensions-socketmmi
 opkg install enigma2-plugin-systemplugins-recordinfobar enigma2-plugin-extensions-icefilms enigma2-plugin-extensions-cubic_streamer
 opkg install enigma2-plugin-extensions-cooltvguide enigma2-plugin-extensions-pipservicerelation.mod enigma2-plugin-extensions-dreamexplorer
-opkg install enigma2-plugin-extensions-infobartunerstate-mod enigma2-plugin-extensions-flashexpander enigma2-plugin-extensions-tvcharts
+opkg install enigma2-plugin-extensions-infobartunerstate 
 echo
 echo "done ... but not complete"
 echo "install ice-network-tuner if you have a box with 2 cpu"
