064fcdb9a0df03d8ef716742857847eeb694e4bf
Koivo <koivo@live.de>
2013-03-31 20:00:38 +0200

    remove some plugins from teamimagescript

--- usr/lib/enigma2/python/Plugins/Extensions/HDF-Toolbox/scripts/create_teamimage.sh
+++ usr/lib/enigma2/python/Plugins/Extensions/HDF-Toolbox/scripts/create_teamimage.sh
@@ -6,26 +6,24 @@ echo
 echo "please wait".
 echo
 echo "remove old plugins"
-opkg remove enigma2-plugin-extensions-remotestreamconvert enigma2-plugin-systemplugins-extnumberzap
+opkg remove enigma2-plugin-extensions-remotestreamconvert
 opkg install kernel-module-nfs-layout-nfsv41-files
 echo
 echo "install system plugins"
 opkg install enigma2-plugin-extensions-bootvideo-hdf enigma2-plugin-extensions-zaphistorybrowser.mod enigma2-plugin-extensions-virtualzap.mod
-opkg install enigma2-plugin-extensions-2ib enigma2-plugin-extensions-et-portal enigma2-plugin-extensions-openairplaymod
+opkg install enigma2-plugin-extensions-2ib enigma2-plugin-extensions-et-portal enigma2-plugin-extensions-openairplaymod enigma2-plugin-systemplugins-extnumberzap
 opkg install enigma2-plugin-extensions-seekbarmod enigma2-plugin-extensions-webinterface-old-package enigma2-plugin-extensions-menusort
 opkg install enigma2-plugin-extensions-jobmanager enigma2-plugin-systemplugins-satelliteequipmentcontrol enigma2-plugin-systemplugins-osd3dmodsetup
 echo
 echo "install addons"
-opkg install enigma2-plugin-extensions-Burnseries enigma2-plugin-extensions-webradiofs enigma2-plugin-extensions-skyrecorder
-opkg install enigma2-plugin-extensions-mediaportal enigma2-plugin-extensions-tvkino enigma2-plugin-extensions-moviecut
-opkg install enigma2-plugin-extensions-songsto enigma2-plugin-extensions-putpattv enigma2-plugin-extensions-netzkino
-opkg install enigma2-plugin-extensions-myentertainment enigma2-plugin-extensions-movie2kserien enigma2-plugin-extensions-enhancedpowersave
-opkg install enigma2-plugin-extensions-movie2kfilme enigma2-plugin-extensions-flashexpander enigma2-plugin-extensions-tvcharts
+opkg install  enigma2-plugin-extensions-webradiofs enigma2-plugin-extensions-skyrecorder enigma2-plugin-extensions-pipservicerelation.mod 
+opkg install enigma2-plugin-extensions-mediaportal enigma2-plugin-extensions-moviecut install enigma2-plugin-extensions-cooltvguide 
+opkg install enigma2-plugin-extensions-putpattv enigma2-plugin-extensions-cacheflush install enigma2-plugin-extensions-infobartunerstate 
+opkg install enigma2-plugin-extensions-movie2kserien enigma2-plugin-extensions-enhancedpowersave
+opkg install enigma2-plugin-extensions-flashexpander enigma2-plugin-extensions-tvcharts enigma2-plugin-extensions-dreamexplorer
 opkg install enigma2-plugin-extensions-dokumonster enigma2-plugin-extensions-cinestreamer enigma2-plugin-extensions-remotechannelstreamimport
 opkg install enigma2-plugin-extensions-epgrefresh_mod enigma2-plugin-extensions-yampmusicplayer enigma2-plugin-extensions-socketmmi
 opkg install enigma2-plugin-systemplugins-recordinfobar enigma2-plugin-extensions-icefilms enigma2-plugin-extensions-cubic_streamer
-opkg install enigma2-plugin-extensions-cooltvguide enigma2-plugin-extensions-pipservicerelation.mod enigma2-plugin-extensions-dreamexplorer
-opkg install enigma2-plugin-extensions-infobartunerstate enigma2-plugin-extensions-cacheflush
 echo
 echo "done ... but not complete"
 echo "install ice-network-tuner if you have a box with 2 cpu"
