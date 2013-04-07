d52f38b2e0d739214eef48be13418d33ac850b0a
Koivo <koivo@live.de>
2013-04-07 20:14:26 +0200

    change teamimagecreate script

--- usr/lib/enigma2/python/Plugins/Extensions/HDF-Toolbox/scripts/create_teamimage.sh
+++ usr/lib/enigma2/python/Plugins/Extensions/HDF-Toolbox/scripts/create_teamimage.sh
@@ -5,6 +5,10 @@ echo "This script create a teamimage ... almost but not complete"
 echo
 echo "please wait".
 echo
+cp /usr/share/enigma2/skin_default/spinner/* /usr/share/enigma2/spinner/ > /dev/null 2>&1
+rm -rf /usr/share/enigma2/skin_default/spinner/* > /dev/null 2>&1
+rmdir /usr/share/enigma2/skin_default/spinner/ > /dev/null 2>&1
+ln -s /usr/share/enigma2/spinner /usr/share/enigma2/skin_default/spinner
 echo "remove old plugins"
 opkg remove enigma2-plugin-extensions-remotestreamconvert
 opkg install kernel-module-nfs-layout-nfsv41-files
@@ -16,9 +20,9 @@ opkg install enigma2-plugin-extensions-seekbarmod enigma2-plugin-extensions-webi
 opkg install enigma2-plugin-extensions-jobmanager enigma2-plugin-systemplugins-satelliteequipmentcontrol enigma2-plugin-systemplugins-osd3dmodsetup
 echo
 echo "install addons"
-opkg install  enigma2-plugin-extensions-webradiofs enigma2-plugin-extensions-skyrecorder enigma2-plugin-extensions-pipservicerelation.mod 
+opkg install enigma2-plugin-extensions-webradiofs enigma2-plugin-extensions-skyrecorder enigma2-plugin-extensions-pipservicerelation.mod 
 opkg install enigma2-plugin-extensions-mediaportal enigma2-plugin-extensions-moviecut install enigma2-plugin-extensions-cooltvguide 
-opkg install enigma2-plugin-extensions-putpattv enigma2-plugin-extensions-cacheflush install enigma2-plugin-extensions-infobartunerstate 
+opkg install enigma2-plugin-extensions-cacheflush install enigma2-plugin-extensions-infobartunerstate 
 opkg install enigma2-plugin-extensions-movie2kserien enigma2-plugin-extensions-enhancedpowersave
 opkg install enigma2-plugin-extensions-flashexpander enigma2-plugin-extensions-tvcharts enigma2-plugin-extensions-dreamexplorer
 opkg install enigma2-plugin-extensions-dokumonster enigma2-plugin-extensions-cinestreamer enigma2-plugin-extensions-remotechannelstreamimport
