b335a560f7b2685079a5ddec48c9727da7ad1aed
Koivo <koivo@live.de>
2013-06-25 17:29:42 +0200

    add script to fix openhdf-version-info

---
+++ usr/lib/enigma2/python/Plugins/Extensions/HDF-Toolbox/scripts/fix_version_info.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+echo "fix openhdf version info ... please wait"
+opkg update > /dev/null 2>&1
+sleep 1
+opkg remove --force-depends openhdf-version-info > /dev/null 2>&1
+sleep 1
+opkg update > /dev/null 2>&1
+sleep 1
+opkg install openhdf-version-info > /dev/null 2>&1
+echo
+cat /etc/image-version
+echo
+echo
+
+exit 0 
