d6fa1a977689ebb9f190ee0ac5d5e40653e9a4f0
Koivo <koivo@live.de>
2013-08-12 21:13:56 +0200

    little change

--- usr/lib/enigma2/python/Plugins/Extensions/HDF-Toolbox/scripts/create_teamimage.sh
+++ usr/lib/enigma2/python/Plugins/Extensions/HDF-Toolbox/scripts/create_teamimage.sh
@@ -8,7 +8,6 @@ echo
 ln -s /usr/share/enigma2/spinner /usr/share/enigma2/skin_default/spinner
 echo "remove old plugins"
 opkg remove enigma2-plugin-extensions-remotestreamconvert
-#opkg install kernel-module-nfs-layout-nfsv41-files
 echo
 echo "install system plugins"
 opkg install enigma2-plugin-extensions-bootvideo-hdf \
