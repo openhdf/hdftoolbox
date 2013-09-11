d14369bdefc346405fbdf2600cf13c506f241a42
Koivo <koivo@live.de>
2013-09-11 18:41:08 +0200

    another speedtest script for flash?

---
+++ usr/lib/enigma2/python/Plugins/Extensions/HDF-Toolbox/scripts/speed_flash.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+target=/usr/share/
+
+echo "speedtest use = $target"
+echo
+
+echo "speedtest $target ~ 1MB ... please wait"
+time dd if=/dev/zero of=$target/blanks2 bs=1024k count=1
+rm $target/blanks2
+echo
+
+echo "speedtest $target ~ 5MB ... please wait"
+dd if=/dev/zero of=$target/blanks2 bs=1024k count=5
+rm $target/blanks2
+echo
+
+echo "speedtest $target ~ 10MB ... please wait"
+time dd if=/dev/zero of=$target/blanks2 bs=1024k count=10
+rm $target/blanks2
+echo
+
+exit 0   
