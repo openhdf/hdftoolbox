1c011386a8c2306d85c6bf39ac765db8c678d625
Koivo <koivo@live.de>
2013-10-17 20:25:12 +0200

    add option to install old/new version of et-hbbtv

--- usr/lib/enigma2/python/Plugins/Extensions/HDF-Toolbox/scripts/et-hbbtv-old.sh
+++ usr/lib/enigma2/python/Plugins/Extensions/HDF-Toolbox/scripts/et-hbbtv-old.sh
@@ -12,7 +12,7 @@ if [ -f /usr/local/browser/browser ]; then
 	df -h | grep /usr
 	echo
 else
-	echo "HbbTV Browser not found ... install ETXx00 2.0.0 HbbTV"
+	echo "HbbTV Browser not found ... install 1.6.1 ETXx00 HbbTV"
 	#freespace=`df -h | grep rootfs | df -h | grep rootfs | cut -c 46-47`
 	freespace=`df | awk '/rootfs/ {print $4}'`
 	freespace2=`df | awk '/usr/ {print $4}'`
@@ -20,12 +20,12 @@ else
 	echo
 	if [ $freespace -ge $freeneeded ]; then
 		echo "$freespace kB available on ubi0:rootfs. $freeneeded kB are needed for installation of HbbTV"
-		opkg install enigma2-plugin-extensions-et-hbbtv  > /dev/null 2>&1
+		opkg install enigma2-plugin-extensions-et-hbbtv-old  > /dev/null 2>&1
 		echo "done"
 	elif [ $freespace2 -ge $freeneeded ]; then
 		echo "$freespace2 kB available on /usr. $freeneeded kB are needed for installation of HbbTV"
 		echo
-		opkg install enigma2-plugin-extensions-et-hbbtv  > /dev/null 2>&1
+		opkg install enigma2-plugin-extensions-et-hbbtv-old  > /dev/null 2>&1
 		echo "done"
 	else
 		echo "You need $freeneeded kB in your Flash available. But it is only $freespace kB free"
