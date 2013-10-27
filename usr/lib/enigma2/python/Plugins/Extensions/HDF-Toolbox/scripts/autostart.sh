bfead3891d0ce7fb8b90668ba25e589eea2d0855
Koivo <koivo@live.de>
2013-10-27 07:13:27 +0100

    correct autostart script

--- usr/lib/enigma2/python/Plugins/Extensions/HDF-Toolbox/scripts/autostart.sh
+++ usr/lib/enigma2/python/Plugins/Extensions/HDF-Toolbox/scripts/autostart.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
-#set temp output file
+echo
+echo "####################### running HDFreaks autostart scripts ####################### "
+echo
+
+##set temp output file
 rm /tmp/hdf.txt
 touch /tmp/hdf.txt
 cat /proc/stb/info/boxtype >> /tmp/hdf.txt
@@ -12,8 +16,8 @@ echo "" >> /tmp/hdf.txt
 cat /proc/stb/info/chipset >> /tmp/hdf.txt
 echo "" >> /tmp/hdf.txt
 cat /proc/cpuinfo >> /tmp/hdf.txt
-####
 
+##use transponder or ntd date and time
 if [ -f /usr/lib/enigma2/python/Plugins/Extensions/HDF-Toolbox/scripts/dvbdate ]; then
 	if [ -f /etc/enigma2/.transponderupdate ]; then
 			echo "update time & date from multiplex"
@@ -39,30 +43,40 @@ fi
 ##reinstall mediaportal skins after update
 echo
 mediaportal=`opkg list-installed | grep extensions-mediaportal | cut -d" " -f1`
-if [ $mediaportal == enigma2-plugin-extensions-mediaportal ]; then
-	echo "mediaportal is installed"
+if [ -z $mediaportal ]; then
+	echo "mediaportal is currently not installed"
+else
 	IP=hdfreaks.cc
 	ping -c 1 $IP  > /dev/null 2>&1
 		if [ $? == 0 ]; then
-			echo "box online"
+			echo -n "box online ... "
 				if [ -f /usr/lib/enigma2/python/Plugins/Extensions/MediaPortal/skins/weed_/haupt_Screen.xml ]; then
 					echo "mediaportal skins are installed ... nothing to do"
 				else
-					echo "missing mediaportal skin ... install now"
+					echo -n "missing mediaportal skin ... install now"
 					opkg install --force-reinstall enigma2-plugin-skins-mediaportal > /dev/null 2>&1
-					echo "done"
+					echo " ... done"
 				fi
 		else
 			echo "box offline"
 		fi
-else
-	echo "mediaportal is currently not installed"
 fi
 
 ##remove old videomode if new screen videomode is installed
-if [ -f /usr/lib/enigma2/python/Screens/VideoMode.pyo ]; then
-	echo "remove old videomode version if exists"
-	rm -fr /usr/lib/enigma2/python/Plugins/SystemPlugins/Videomode/ > /dev/null 2>&1
+echo
+videomode=`opkg list-installed | grep systemplugins-videomode | cut -d" " -f1`
+if [ -z $videomode ]; then
+	echo "no old videomode found ... nothing to do"
 else
-	echo "nothing to do"
+	if [ -f /usr/lib/enigma2/python/Screens/VideoMode.pyo ]; then
+		echo "remove old videomode version"
+		opkg remove --force-depends enigma2-plugin-systemplugins-videomode > /dev/null 2>&1
+		rm -fr /usr/lib/enigma2/python/Plugins/SystemPlugins/Videomode/ > /dev/null 2>&1
+	else
+		echo "no old videomode found ... nothing to do"
+	fi
 fi
+
+
+
+echo
