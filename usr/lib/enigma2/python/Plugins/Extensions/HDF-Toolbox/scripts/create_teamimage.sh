8b6b28e496ffe0af80b26c78ee2cf578dbf7b98f
Koivo <koivo@live.de>
2014-05-01 19:31:21 +0200

    optimize update @ console

--- usr/lib/enigma2/python/Plugins/Extensions/HDF-Toolbox/scripts/create_teamimage.sh
+++ usr/lib/enigma2/python/Plugins/Extensions/HDF-Toolbox/scripts/create_teamimage.sh
@@ -12,27 +12,26 @@ echo
 echo "install system plugins"
 opkg install enigma2-plugin-extensions-bootvideo \
 sqlite3 \
-enigma2-plugin-extensions-zaphistorybrowser.mod \
-enigma2-plugin-extensions-virtualzap.mod \
-enigma2-plugin-extensions-openairplaymod \
 enigma2-plugin-systemplugins-extnumberzap \
-enigma2-plugin-extensions-webinterface-old-package \
-enigma2-plugin-extensions-picturecenterfs \
-enigma2-plugin-extensions-menusort \
-enigma2-plugin-extensions-jobmanager \
-enigma2-plugin-extensions-mediainfo \
 enigma2-plugin-systemplugins-crossepg \
 enigma2-plugin-systemplugins-satelliteequipmentcontrol \
-enigma2-plugin-extensions-xbmcwetter \
-enigma2-plugin-extensions-customsubservices \
-enigma2-plugin-extensions-audiosync \
+enigma2-plugin-systemplugins-recordinfobar \
+enigma2-plugin-systemplugins-vixbackuptools \
 enigma2-plugin-systemplugins-osd3dmodsetup
 echo
 echo "install extensions"
 opkg install enigma2-plugin-extensions-webradiofs \
 enigma2-plugin-extensions-skyrecorder \
 enigma2-plugin-extensions-mediaportal \
+enigma2-plugin-extensions-zaphistorybrowser.mod \
+enigma2-plugin-extensions-virtualzap.mod \
+enigma2-plugin-extensions-openairplaymod \
 enigma2-plugin-extensions-picturecenterfs \
+enigma2-plugin-extensions-webinterface-old-package \
+enigma2-plugin-extensions-picturecenterfs \
+enigma2-plugin-extensions-menusort \
+enigma2-plugin-extensions-jobmanager \
+enigma2-plugin-extensions-mediainfo \
 enigma2-plugin-extensions-infobartunerstate \
 enigma2-plugin-extensions-enhancedpowersave \
 enigma2-plugin-extensions-flashexpander \
@@ -41,7 +40,9 @@ enigma2-plugin-extensions-dreamexplorer \
 enigma2-plugin-extensions-remotechannelstreamimport \
 enigma2-plugin-extensions-epgrefresh_mod \
 enigma2-plugin-extensions-yampmusicplayer \
-enigma2-plugin-systemplugins-recordinfobar \
+enigma2-plugin-extensions-xbmcwetter \
+enigma2-plugin-extensions-customsubservices \
+enigma2-plugin-extensions-audiosync \
 enigma2-plugin-extensions-tvspielfilm
 echo
 
@@ -49,7 +50,7 @@ echo "check box now"
 line=$(grep ixuss /etc/enigma2/boxinformations)
 if [ -n $? ]; then
 	echo $line
-	opkg install enigma2-plugin-systemplugins-ice-network-tuner
+	#opkg install enigma2-plugin-systemplugins-ice-network-tuner
 	opkg install enigma2-plugin-extensions-moviecut
 	opkg install enigma2-plugin-extensions-pipservicerelation.mod
 else
--- usr/lib/enigma2/python/Plugins/Extensions/HDF-Toolbox/scripts/opkg_update.sh
+++ usr/lib/enigma2/python/Plugins/Extensions/HDF-Toolbox/scripts/opkg_update.sh
@@ -1,12 +1,27 @@
 #!/bin/sh
 
 cd /tmp
-echo "Download and install update"
 echo
-echo "starting okgp update & upgrade"
+echo "Download and install updates."
 opkg update  > /dev/null 2>&1
-opkg upgrade
 echo
-echo "Update Done ... Please reboot your Box now!"
+opkg list-upgradable
+if [ -n $? ]; then
+	echo
+	echo "Nothing to upgrade at this point."
+	echo
+	echo
+else
+	echo
+	echo "Please wait ... update is starting now."
+	echo "Don't stop this, close the windows or switch off your box!"
+	echo "Update is done, if you can close this windows with OK button."
+	echo
+	opkg upgrade > /dev/null 2>&1
+	echo
+	echo "Update Done ... Please reboot your Box now!"
+	echo
+	echo
+fi
 
-exit 0 
+exit 0
