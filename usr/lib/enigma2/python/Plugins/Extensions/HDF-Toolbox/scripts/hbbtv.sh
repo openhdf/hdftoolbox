b9f8b98e445449bc15b9368cbc25f1d1cb84f1dd
Koivo <koivo@live.de>
2014-05-06 21:06:27 +0200

    start/stop hbbtv under tools

--- usr/lib/enigma2/python/Plugins/Extensions/HDF-Toolbox/scripts/hbbtv.sh
+++ usr/lib/enigma2/python/Plugins/Extensions/HDF-Toolbox/scripts/hbbtv.sh
@@ -1,52 +1,19 @@
 #!/bin/sh
 
-cd /tmp
-
-if [ -f /etc/enigma2/bookmarks.cfg ]; then 
+if [ -x /usr/local/NXBrowser/launcher ]; then 
 	echo
-	echo "hbbtv bookmarks found"
-	cd /etc/enigma2
-	echo "make backup"
-	mv /etc/enigma2/bookmarks.cfg /etc/enigma2/bookmarks.cfg.ori
-	wget http://addons.hdfreaks.cc/et9000/hbbtv/bookmarks.cfg
-	echo "Update Done ... "
+	/usr/local/NXBrowser/launcher stop > /dev/null 2>&1
+	chmod 644 /usr/local/NXBrowser/launcher
+	echo "HbbTV is now disabled."
+	echo "Please restart Enigma now."
+	echo
+	echo
 else
 	echo
-	echo "no hbbtv plugin found"
+	chmod 755 /usr/local/NXBrowser/launcher
+	echo "HbbTV is now enabled."
+	echo "Please restart Enigma now."
 	echo
 	echo
 fi
-
-if [ -f /etc/enigma2/bookmarks_private.cfg ]; then 
-	echo
-	echo "hbbtv bookmarks_private.cfg found"
-	cd /etc/enigma2
-	echo "make backup"
-	mv /etc/enigma2/bookmarks_private.cfg /etc/enigma2/bookmarks_private.cfg.ori
-	wget http://addons.hdfreaks.cc/et9000/hbbtv/bookmarks_private.cfg
-	echo "Update Done ... "
-else
-	echo
-	echo "no hbbtv plugin found"
-	echo
-	echo
-fi
-
-#if [ -f /usr/lib/enigma2/python/Plugins/SystemPlugins/WebBrowser/plugin.py ]; then 
-#	echo
-#	echo "hbbtv plugin found"
-#	cd /usr/lib/enigma2/python/Plugins/SystemPlugins/WebBrowser
-#	echo "make backup & install icewaere plugin"
-#	mv /usr/lib/enigma2/python/Plugins/SystemPlugins/WebBrowser/plugin.py /usr/lib/enigma2/python/Plugins/SystemPlugins/WebBrowser/plugin.py.ori
-#	wget http://addons.hdfreaks.cc/et9000/hbbtv/plugin.py
-#	echo "Update Done ... Please restart Enigma now"
-#	echo
-#	echo
-#else
-#	echo
-#	echo "no hbbtv plugin found"
-#	echo
-#	echo
-#fi
-
 exit 0 
