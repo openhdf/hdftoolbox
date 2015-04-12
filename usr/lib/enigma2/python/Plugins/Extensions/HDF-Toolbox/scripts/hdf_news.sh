ea2ef96ade5443fa8a00377360b778185d699b85
Koivo <koivo@live.de>
2015-04-12 10:17:53 +0200

    add openhdf news

---
+++ usr/lib/enigma2/python/Plugins/Extensions/HDF-Toolbox/scripts/hdf_news.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+cd /tmp
+rm -f /tmp/news.txt
+wget http://feeds.hdfreaks.cc/news.txt > /dev/null 2>&1
+
+if [ -f /tmp/news.txt ]; then 
+		cat /tmp/news.txt
+		echo
+else
+		echo
+		echo "Sorry, there are currently no HDF news available"
+		echo
+fi
+
+exit 0 
