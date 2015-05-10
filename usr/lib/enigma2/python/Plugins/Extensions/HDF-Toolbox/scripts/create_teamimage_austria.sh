b037f41358c53877c6112db66d48a3d1d7c83e33
Koivo <koivo@live.de>
2015-05-10 17:46:34 +0200

    fix teamimage scripts

--- usr/lib/enigma2/python/Plugins/Extensions/HDF-Toolbox/scripts/create_teamimage_austria.sh
+++ usr/lib/enigma2/python/Plugins/Extensions/HDF-Toolbox/scripts/create_teamimage_austria.sh
@@ -14,7 +14,7 @@ opkg remove enigma2-plugin-extensions-remotestreamconverter \
 opkg remove enigma2-plugin-extensions-remotechannelstreamimport
 echo
 echo "install system plugins"
-sqlite3 \
+opkg install sqlite3 \
 enigma2-plugin-systemplugins-extnumberzap \
 enigma2-plugin-systemplugins-satelliteequipmentcontrol \
 enigma2-plugin-systemplugins-weathercomponenthandler
