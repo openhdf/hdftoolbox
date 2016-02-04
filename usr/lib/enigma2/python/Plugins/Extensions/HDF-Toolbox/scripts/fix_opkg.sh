7560a45983baab01e933d6ed50b671c8397f46c8
Koivo <koivo@live.de>
2016-02-04 19:44:11 +0100

    extend opkg fix (reverted from commit 7031a546ca0b759b58997fd90f3c30c9d6e9557c)

--- usr/lib/enigma2/python/Plugins/Extensions/HDF-Toolbox/scripts/fix_opkg.sh
+++ usr/lib/enigma2/python/Plugins/Extensions/HDF-Toolbox/scripts/fix_opkg.sh
@@ -2,14 +2,10 @@
 
 echo -e "\nfix opkg package problems"
 sleep 1
-find /var/lib/opkg/status -type f -exec sed -i 's/Version: 1.6.0-r0/Version: 1.6.1-r0/' {} \;
-find /var/lib/opkg/status -type f -exec sed -i 's/Conflicts: /#Conflicts:/' {} \;
-find /var/lib/opkg/status -type f -exec sed -i 's/Version: 1.6.1-r0/Version: 1.7.1-r0/' {} \;
-find /var/lib/opkg/status -type f -exec sed -i 's/Version: 1.7+git14825+3674742/Version: 1.7.1-r0/' {} \;
+sed -i 's/Version: 1.6.0-r0/Version: 1.6.1-r0/' /var/lib/opkg/status
+sed -i 's/Conflicts: /#Conflicts:/' /var/lib/opkg/status
 echo -e "\nopkg update"
 opkg update
-opkg --force-depends --force-reinstall install libgstvideo-1.0-0 libgstriff-1.0-0 libgstcodecparsers-1.0-0 libgstadaptivedemux-1.0-0 gstreamer1.0 libgsttag-1.0-0 libgstsdp-1.0-0 libgsturidownloader-1.0-0 libgstrtsp-1.0-0 gstreamer1.0-plugins-good gstreamer1.0-plugin-dvbmediasink gstreamer1.0-plugins-good
-opkg flag ok libgstvideo-1.0-0 libgstriff-1.0-0 libgstcodecparsers-1.0-0 libgstadaptivedemux-1.0-0 gstreamer1.0 libgsttag-1.0-0 libgstsdp-1.0-0 libgsturidownloader-1.0-0 libgstrtsp-1.0-0 gstreamer1.0-plugins-good gstreamer1.0-plugin-dvbmediasink gstreamer1.0-plugins-good
 echo -e "done\nopkg upgrade"
 opkg upgrade
 echo -e "done\n\nand now we must restart enigma!"
