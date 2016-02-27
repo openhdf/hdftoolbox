0d3bec5717f77837179a894ce4a5be8bd3fbd21a
Koivo <koivo@live.de>
2016-02-27 13:21:20 +0100

    fix bouquets_b.sh ^^

--- usr/lib/enigma2/python/Plugins/Extensions/HDF-Toolbox/scripts/bouquets_b.sh
+++ usr/lib/enigma2/python/Plugins/Extensions/HDF-Toolbox/scripts/bouquets_b.sh
@@ -1,15 +1,9 @@
 #!/bin/sh
 
 cd /etc/enigma2/
-echo "lamedb" > /tmp/hdf_b.cfg
 ls lamedb *bouquet* *.xml >/tmp/hdf_b.cfg
 
 echo -e "Backup your Bouquets/Timers/Automounts from /etc/enigma2/ to HDD\n"
-echo
 [ -d /hdd/backup ] || mkdir /hdd/backup
-	tar -czf /hdd/backup/HDF_Backup.tar.gz --files-from=/tmp/hdf_b.cfg 2> /dev/null
-echo
-echo "Backup complete"
+tar -czf /hdd/backup/HDF_Backup.tar.gz --files-from=/tmp/hdf_b.cfg 2> /dev/null
 echo -e "\nBackup complete\nYou can find your Backup now in /hdd/backup/HDF_Backup.tar.gz\n\n"
-echo
-echo
