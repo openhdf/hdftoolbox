6f177f2c64e66043add12e6f5251b18bef47bbc7
Koivo <koivo@live.de>
2016-05-24 17:29:43 +0200

    update iptv downloader

---
+++ usr/lib/enigma2/python/Plugins/Extensions/HDF-Toolbox/scripts/IPTV.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+echo "Here you can find the current IPTV and webcam streams"
+echo "----------------------------------------------------------"
+echo "To keep up to date the lists we are relies on your help."
+echo "If you found streams in the lists, they do not run correct" 
+echo "for several days, write this in HDF forum."
+echo "----------------------------------------------------------"
+echo "If you will share new IPTV or webcam streams," 
+echo "write a PM to pixbox or Googgi"
+echo " "
+exit 1
+
