e485da85a18e6b9b4f726c40df2438c79fe0a9a0
Koivo <koivo@live.de>
2017-02-16 20:28:14 +0100

    [bouquets_r.sh] update by nomjasV

--- usr/lib/enigma2/python/Plugins/Extensions/HDF-Toolbox/scripts/bouquets_r.sh
+++ usr/lib/enigma2/python/Plugins/Extensions/HDF-Toolbox/scripts/bouquets_r.sh
@@ -5,14 +5,13 @@ echo -e "Restore your Bouquets/Timers/Automounts from HDD to /etc/enigma2/\n"
 if [ -f /hdd/backup/HDF_Backup.tar.gz ]; then 
 	echo "Backup found ... restore now"
 	cd /etc/enigma2
-	rm -f *bouquet* lamedb automounts.xml *timer*
-	tar xzf /hdd/backup/HDF_Backup.tar.gz
-	echo -e "\nRestore complete\nPlease wait ... restart GUI"
-	sleep 5
 	init 4
 	sleep 1
+	rm -f *bouquet* lamedb automounts.xml *timer*
+	tar xzf /hdd/backup/HDF_Backup.tar.gz
+	echo -e "\nRestore complete\nPlease wait ... restart GUI\n\n"
+	sleep 2
 	init 3
-	echo -e "\n\n"
 else
 	echo -e "No backup /hdd/backup/HDF_Backup.tar.gz found"\!"\nCan't restore old data\n"
 fi
