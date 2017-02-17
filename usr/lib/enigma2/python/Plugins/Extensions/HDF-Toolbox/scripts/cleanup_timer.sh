8506dd3f7c29db06fd63c79b06aa63c3a3922df5
Koivo <koivo@live.de>
2017-02-17 20:59:44 +0100

    cleanup_timer.sh fix by nomjasV

--- usr/lib/enigma2/python/Plugins/Extensions/HDF-Toolbox/scripts/cleanup_timer.sh
+++ usr/lib/enigma2/python/Plugins/Extensions/HDF-Toolbox/scripts/cleanup_timer.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 
 echo -e "\nfind <log code= in /etc/enigma2/timers.xml and remove this"
-sleep 5
-sed -i '/<log code="/d' /etc/enigma2/timers.xml
-echo -e "\nand now we must restart enigma!"
-sleep 4
-killall enigma2
-sleep 2
-echo
+sleep 1
+echo -e "\nand now we must restart enigma!\n"
+init 4
+sleep 1
+sed -i '/log code\=/d' /etc/enigma2/timers.xml
+init 3
 
 exit 0   
