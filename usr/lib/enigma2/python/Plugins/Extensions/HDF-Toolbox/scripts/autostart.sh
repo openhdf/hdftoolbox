1f38417d7b73c311985d70c771c191aaf717b518
Koivo <koivo@live.de>
2017-02-17 19:18:25 +0100

    [autostart.sh] modification by nomjasV

--- usr/lib/enigma2/python/Plugins/Extensions/HDF-Toolbox/scripts/autostart.sh
+++ usr/lib/enigma2/python/Plugins/Extensions/HDF-Toolbox/scripts/autostart.sh
@@ -15,36 +15,16 @@ echo "" >> /tmp/hdf.txt
 cat /proc/cpuinfo >> /tmp/hdf.txt
 
 #check image version and write to issue.net
-imageversion=`grep ^getImageVersion /tmp/.ImageVersion | cut -d" " -f3`
+imageversion=`awk '/getImageVersion/ {print $3}' /tmp/.ImageVersion`
 imagebuild=`grep ^version /etc/image-version | cut -f2 -d=`
 if ! grep "$imageversion build: $imagebuild" /etc/issue.net >/dev/null ;then
 	sed -i "s|^~ HDFreaks Image.*|~ HDFreaks Image V$imageversion build: $imagebuild ~|" /etc/issue.net
 fi
 
 #check spinner symlink
-if [ -L /usr/share/enigma2/skin_default/spinner ]; then
-		echo "spinner symlink found"
-	else
-		echo "create spinner symlink"
-		rm -fr /usr/share/enigma2/skin_default/spinner > /dev/null 2>&1
-		ln -s /usr/share/enigma2/spinner/ /usr/share/enigma2/skin_default/
-fi
-
-##remove old videomode if new screen videomode is installed
-videomode=`opkg list-installed | grep systemplugins-videomode | cut -d" " -f1`
-if [ -z $videomode ]; then
-	echo "no old videomode found ... nothing to do"
-else
-	if [ -f /usr/lib/enigma2/python/Screens/VideoMode.pyo ]; then
-		echo "remove old videomode version"
-		opkg remove --force-depends enigma2-plugin-systemplugins-videomode > /dev/null 2>&1
-		rm -fr /usr/lib/enigma2/python/Plugins/SystemPlugins/Videomode/ > /dev/null 2>&1
-	else
-		echo "no old videomode found ... nothing to do"
-	fi
-fi
+ln -fs /usr/share/enigma2/spinner/ /usr/share/enigma2/skin_default/.
 
 ##create iptv symlinks to /usr/scripts/ for cronjobs
 echo "check scripts and create symlinks"
 [ -d /usr/scripts ] || mkdir /usr/scripts
-ln -s /usr/lib/enigma2/python/Plugins/Extensions/HDF-Toolbox/scripts/IPTV_* /usr/scripts/ > /dev/null 2>&1
+ln -fs /usr/lib/enigma2/python/Plugins/Extensions/HDF-Toolbox/scripts/IPTV_* /usr/scripts/.
