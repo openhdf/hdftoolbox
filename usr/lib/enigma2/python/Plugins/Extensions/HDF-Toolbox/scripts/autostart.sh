8dcf57623a198e7741eea9d3703e5357979133f0
Koivo <koivo@live.de>
2017-08-26 19:27:12 +0200

    update autostart.sh thanks@nomjasV

--- usr/lib/enigma2/python/Plugins/Extensions/HDF-Toolbox/scripts/autostart.sh
+++ usr/lib/enigma2/python/Plugins/Extensions/HDF-Toolbox/scripts/autostart.sh
@@ -5,10 +5,9 @@ echo "####################### running HDFreaks autostart scripts ###############
 echo
 
 ##set temp output file
-cat /proc/stb/info/boxtype > /tmp/hdf.txt
-cat /proc/version >> /tmp/hdf.txt
+cat /proc/stb/info/boxtype /proc/version > /tmp/hdf.txt
 echo "" >> /tmp/hdf.txt
-less /etc/version >> /tmp/hdf.txt
+cat /etc/version >> /tmp/hdf.txt
 echo "" >> /tmp/hdf.txt
 cat /proc/stb/info/chipset >> /tmp/hdf.txt
 echo "" >> /tmp/hdf.txt
@@ -18,7 +17,7 @@ cat /proc/cpuinfo >> /tmp/hdf.txt
 imageversion=`awk '/getImageVersion/ {print $3}' /tmp/.ImageVersion`
 imagebuild=`grep ^version /etc/image-version | cut -f2 -d=`
 if ! grep "$imageversion build: $imagebuild" /etc/issue.net >/dev/null ;then
-	sed -i "s|^~ HDFreaks Image.*|~ HDFreaks Image V$imageversion build: $imagebuild ~|" /etc/issue.net
+	sed -i "s|^~ HDFreaks Image.*|~ HDFreaks Image V$imageversion Build #$imagebuild ~|" /etc/issue.net
 fi
 
 #check spinner symlink
@@ -26,7 +25,7 @@ ln -fs /usr/share/enigma2/spinner/ /usr/share/enigma2/skin_default/.
 
 #check ld-linux symlink
 python /usr/lib/enigma2/python/BoxBrandingTest.pyo | sed 's/<$//g;s/ /_/g' > /tmp/boxbranding.cfg
-if grep -Eqs 'cortexa15hf-neon-vfpv4' cat /tmp/boxbranding.cfg; then
+if grep -Eqs 'cortexa15hf-neon-vfpv4' /tmp/boxbranding.cfg; then
 	ln -fs /lib/ld-linux-armhf.so.3 /lib/ld-linux.so.3
 fi
 
