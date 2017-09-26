996d3650e248c2dc093b35158c529efd72722a5b
Koivo <koivo@live.de>
2017-09-26 17:53:50 +0200

    update fix samba configs script

--- usr/lib/enigma2/python/Plugins/Extensions/HDF-Toolbox/plugin.py
+++ usr/lib/enigma2/python/Plugins/Extensions/HDF-Toolbox/plugin.py
@@ -22,7 +22,7 @@ import NavigationInstance
 from downloader import Hdf_Downloader
 from boxbranding import getBoxType, getMachineBrand, getMachineName, getDriverDate, getImageVersion, getImageBuild, getBrandOEM
 
-toolboxversion = "Toolbox Version - 24.09.2017"
+toolboxversion = "Toolbox Version - 26.09.2017"
 
 try:
 	os.system("echo ~~~ Box Info ~~~~~~~~~~~~~~~~~~~~"" > /tmp/.ImageVersion")
--- usr/lib/enigma2/python/Plugins/Extensions/HDF-Toolbox/scripts/fix_samba.sh
+++ usr/lib/enigma2/python/Plugins/Extensions/HDF-Toolbox/scripts/fix_samba.sh
@@ -2,11 +2,15 @@
 
 echo "Download and install Samba4 config files"
 cd /etc/samba/
-curl -O https://raw.githubusercontent.com/oe-alliance/oe-alliance-core/4.1/meta-oe/recipes-connectivity/samba/samba-4.%25/smb.conf > /dev/null 2>&1
+curl -O http://feeds.hdfreaks.cc/extra_files/samba/smb.conf > /dev/null 2>&1
+curl -O http://feeds.hdfreaks.cc/extra_files/samba/smb-user.conf > /dev/null 2>&1
 
 cd /etc/samba/distro/
-curl -O https://raw.githubusercontent.com/oe-alliance/oe-alliance-core/4.1/meta-oe/recipes-connectivity/samba/samba-4.%25/openhdf/smb-shares.conf > /dev/null 2>&1
-curl -O https://raw.githubusercontent.com/oe-alliance/oe-alliance-core/4.1/meta-oe/recipes-connectivity/samba/samba-4.%25/openhdf/smb-branding.conf > /dev/null 2>&1
+curl -O http://feeds.hdfreaks.cc/extra_files/samba/distro/smb-secure.conf > /dev/null 2>&1
+curl -O http://feeds.hdfreaks.cc/extra_files/samba/distro/smb-insecure.conf > /dev/null 2>&1
+curl -O http://feeds.hdfreaks.cc/extra_files/samba/distro/smb-global.conf > /dev/null 2>&1
+curl -O http://feeds.hdfreaks.cc/extra_files/samba/distro/smb-shares.conf > /dev/null 2>&1
+curl -O http://feeds.hdfreaks.cc/extra_files/samba/distro/smb-branding.conf > /dev/null 2>&1
 
 echo
 echo "Update Done ... Please wait ..."
