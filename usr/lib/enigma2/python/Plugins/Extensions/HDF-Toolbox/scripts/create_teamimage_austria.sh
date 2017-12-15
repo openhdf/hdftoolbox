57c3ca8460470f408e053edd9fce0a7773b1e7c5
koivo <koivo@live.de>
2017-12-15 14:41:18 +0100

    create_teamimage_austria.sh update

--- usr/lib/enigma2/python/Plugins/Extensions/HDF-Toolbox/scripts/create_teamimage_austria.sh
+++ usr/lib/enigma2/python/Plugins/Extensions/HDF-Toolbox/scripts/create_teamimage_austria.sh
@@ -1,52 +1,44 @@
 #!/bin/sh
 ##Teamimage Script by HDFreaks.cc
-##googgi - 01.10.2015
+##googgi - 15.12.2017
 
 echo "Ein Teamimage Austria Edition wird erstellt..."
 echo
-echo "(c) 2015 by googgi"
+echo "(c) by googgi"
 echo
 echo "bitte warten".
 echo
 opkg update > /dev/null 2>&1
-ln -s /usr/share/enigma2/spinner /usr/share/enigma2/skin_default/spinner
-echo "remove preinstalled unwanted plugins"
 echo
 echo "install system plugins"
 opkg install sqlite3 \
-enigma2-plugin-systemplugins-extnumberzap \
-enigma2-plugin-systemplugins-satelliteequipmentcontrol \
-enigma2-plugin-systemplugins-weathercomponenthandler
+enigma2-plugin-systemplugins-extnumberzap
 echo
 echo "install extensions"
-opkg install enigma2-plugin-extensions-audiosync \
-enigma2-plugin-extensions-cooltvguide \
-enigma2-plugin-extensions-customsubservices \
-enigma2-plugin-extensions-dreamexplorer \
-enigma2-plugin-extensions-et-portal \
+opkg install enigma2-plugin-extensions-cooltvguide \
 enigma2-plugin-extensions-infobartunerstate \
+enigma2-plugin-extensions-iptvplayer \
 enigma2-plugin-extensions-mediainfo \
 enigma2-plugin-extensions-mediaportal \
+enigma2-plugin-extensions-mpgz \
+enigma2-plugin-extensions-m3uconverter \
 enigma2-plugin-extensions-piconmanager \
-enigma2-plugin-extensions-remotechannelstreamimport \
 enigma2-plugin-extensions-webradiofs  \
 enigma2-plugin-extensions-werbezapper \
 enigma2-plugin-extensions-xbmcwetter \
 enigma2-plugin-extensions-zaphistorybrowser
 echo
 echo "install austria settings"
-opkg install enigma2-plugin-settings-googgi 
+opkg install enigma2-plugin-settings-austria-19.2-13.0-by-googgi 
 echo
 echo "install nblack51-skin"
 opkg install enigma2-plugin-skins-nblack51-openhdf-mod 
 echo
 echo "Installation erfolgreich abgeschlossen!"
 echo
-echo "Nun bitte neu booten, zuvor aber Hinweise lesen!"
+echo "Hinweis 1: HDF-Toolbox > Skin Selector > Skin wechseln auf: nblack51.openhdf.mod"
 echo
-echo "Hinweis 1: nach dem Reboot Favorit > Sender neu waehlen"
-echo
-echo "Hinweis 2: HDF-Toolbox > Skin Selector > Skin wechseln auf: nblack51.openhdf.mod"
+echo "Hinweis 2: nach dem Reboot die Taste 1 drücken, um in den Favorit AUSTRIA zu kommen"
 echo
 echo "Das Teamimage Austria ist damit fertig"
 echo "Viel Spass damit wuenscht Googgi"
