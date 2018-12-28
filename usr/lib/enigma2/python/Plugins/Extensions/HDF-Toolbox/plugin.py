1241510bad13129255515599f260bb2b57c79d7e
koivo <koivo@live.de>
2018-12-28 19:15:02 +0100

    update austria teamimage script

--- usr/lib/enigma2/python/Plugins/Extensions/HDF-Toolbox/plugin.py
+++ usr/lib/enigma2/python/Plugins/Extensions/HDF-Toolbox/plugin.py
@@ -22,7 +22,7 @@ import NavigationInstance
 from downloader import Hdf_Downloader
 from boxbranding import getBoxType, getMachineBrand, getMachineName, getDriverDate, getImageVersion, getImageBuild, getBrandOEM, getImageType
 
-toolboxversion = "Toolbox Version - 23.12.2018"
+toolboxversion = "Toolbox Version - 28.12.2018"
 
 try:
 	os.system("echo box_type=" + getBoxType() + " > /etc/image-version")
--- usr/lib/enigma2/python/Plugins/Extensions/HDF-Toolbox/scripts/create_teamimage_austria.sh
+++ usr/lib/enigma2/python/Plugins/Extensions/HDF-Toolbox/scripts/create_teamimage_austria.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 ##Teamimage Script by HDFreaks.cc
-##googgi - 15.12.2017
+##googgi - 28.12.2018
 
 echo "Ein Teamimage Austria Edition wird erstellt..."
 echo
@@ -17,11 +17,9 @@ echo
 echo "install extensions"
 opkg install enigma2-plugin-extensions-cooltvguide \
 enigma2-plugin-extensions-infobartunerstate \
-enigma2-plugin-extensions-iptvplayer \
 enigma2-plugin-extensions-mediainfo \
 enigma2-plugin-extensions-mediaportal \
 enigma2-plugin-extensions-mpgz \
-enigma2-plugin-extensions-m3uconverter \
 enigma2-plugin-extensions-piconmanager \
 enigma2-plugin-extensions-webradiofs  \
 enigma2-plugin-extensions-werbezapper \
