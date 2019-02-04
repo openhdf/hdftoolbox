4540d3de99c821970060604ea0e5eeffb7dd9c8e
koivo <koivo@live.de>
2019-02-04 20:09:09 +0100

    fix del_epg script

--- usr/lib/enigma2/python/Plugins/Extensions/HDF-Toolbox/scripts/del_epg.sh
+++ usr/lib/enigma2/python/Plugins/Extensions/HDF-Toolbox/scripts/del_epg.sh
@@ -2,13 +2,24 @@
 
 echo
 echo "find and destroy epg.dat"
-export epgfilename=`find / -name epg.dat`
+export epgfilename2=`find /media/hdd/ -name epg.dat`
+export epgfilename=`find /etc/ -name epg.dat`
 echo $epgfilename
+echo $epgfilename2
 
 if [ -f "$epgfilename" ]; then
 	echo "found epg = $epgfilename"
 	echo "delete epg & restart enigma"
 	rm -f $epgfilename
+	echo "please wait ..."
+	sleep 3
+	init 4
+	sleep 1
+	init 3
+elif [ -f "$epgfilename2" ]; then
+	echo "found epg = $epgfilename2"
+	echo "delete epg & restart enigma"
+	rm -f $epgfilename2
 	echo "please wait ..."
 	sleep 3
 	init 4
