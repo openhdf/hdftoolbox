e214b00750e6cc8b70e9c40c96f754bbcdd9f0a0
koivo <koivo@live.de>
2019-02-17 09:17:30 +0100

    add minilocale

---
+++ usr/lib/enigma2/python/Plugins/Extensions/HDF-Toolbox/scripts/minilocale.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+echo "Install minilocale files"
+IP=hdfreaks.cc
+ping -c 1 $IP  > /dev/null 2>&1
+
+if [ $? == 0 ]
+	then
+		echo
+		opkg update
+		echo
+		opkg install --force-reinstall minilocale
+		echo
+		echo "please restart enigma now"
+		echo
+	else
+		echo
+		echo "sorry ... update failed ... is you box online?"
+		echo
+fi
+
+exit 0 
